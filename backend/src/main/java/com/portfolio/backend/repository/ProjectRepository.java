becbb9f1e33216cfcbdad4cc695bae5554f71d15
adarshrai569-beep <adarshrai569@gmail.com>
2026-02-08 22:11:11 +0530

    Removed backend project from repository

--- backend/src/main/java/com/portfolio/backend/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.portfolio.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.portfolio.backend.entity.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
